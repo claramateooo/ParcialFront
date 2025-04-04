7bb8b99c07829e21cf931642fc08260d0f8da00a
claramateooo <150123384+claramateooo@users.noreply.github.com>
2025-04-04 17:27:40 +0200

    Add files via upload

---
+++ routes/index.tsx
@@ -0,0 +1,10 @@
+import Countryform from "../components/Countryform.tsx";
+
+  export default function HomePage() {
+    return (
+      <div>
+        <a href="/showInfo">Ir al formulario</a>
+      </div>
+    );
+  }
+  
---
+++ types.ts
@@ -0,0 +1,15 @@
+
+export type validatePhone={
+    phone:string;
+    country:string;
+    is_valid:boolean;
+    is_formatted_properly:boolean;
+
+}
+export type City={
+    name:string;
+}
+export type Country={
+    name:string;
+    capital:string;
+}
